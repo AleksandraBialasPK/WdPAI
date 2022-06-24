4811fdece7381b5566cf01dd7c9c10ed8f1f0521
Bialas <aleksandra.bialas@nokia.com>
2022-06-24 12:31:34 +0000

    adding routing php

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends Appcontroller {
+    
+    //Nasze widoki
+    public function index(){
+        // TODO display login.html
+        die("index method");
+    }
+
+    public function project(){
+        //TODO display projcts.html
+        die("project method");
+    }
+}
