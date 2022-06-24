cd349290cca619865c15cde4823a5da1f24ade57
Bialas <aleksandra.bialas@nokia.com>
2022-06-24 15:02:08 +0000

    AppController

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -1,5 +1,18 @@
 <?php
 
+//nasz kontroler bazowy
 class AppController {
-    
+    //funkcja wspolczielona z klasami dziedziczacymi jest protected
+    protected function render(string $template = null) {
+        $templatePath = 'public/views/'.$template.'html';
+        $output = 'File not found.';
+        
+        if(file_exists($templatePath)) {
+            //zapisujemy do bufora
+            ob_start();
+            include $templatePath;
+            $output = ob_get_clean; //wyrzuca zapis bufora
+        }
+        print $output;
+    }
 }
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -7,11 +7,11 @@ class DefaultController extends Appcontroller {
     //Nasze widoki
     public function index() {
         // TODO display login.html
-        die("index method");
+        $this->render('login');
     }
 
     public function project() {
         //TODO display projcts.html
-        die("project method");
+        $this->render('project');
     }
 }
