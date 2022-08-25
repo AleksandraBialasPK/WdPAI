e76a5a1ac23265c595923348845df3c01f9d3a2a
Aleksandra Bialas <aleksandra.bialas@student.pk.edu.pl>
2022-08-25 18:10:39 +0000

    fixing mistakes

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once 'AppController.php';
-require_once __DIR__.'/../models/User.php'
+require_once __DIR__.'/../models/User.php';
 
 class SecurityController extends AppController
 {
     public function login()
     {
-       $user - new User(email:'ktos@pk.edu.pl', password: 'cos', name: 'Jakis', surname: 'Ktos') 
+       $user - new User(email:'ktos@pk.edu.pl', password: 'cos', name: 'Jakis', surname: 'Ktos');
     }
 }
