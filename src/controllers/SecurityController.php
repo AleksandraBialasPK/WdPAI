b4385c35e38637a47257fc87624ee81f598f6ef3
Aleksandra Bialas <aleksandra.bialas@student.pk.edu.pl>
2022-09-05 18:50:29 +0000

    adding an if statement checking whether the input is not blank

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -1,26 +1,29 @@
 <?php
 
 require_once 'AppController.php';
-require_once __DIR__.'/../models/User.php';
+require_once __DIR__ . '/../models/User.php';
 
 class SecurityController extends AppController
 {
     public function login()
     {
-       $user = new User(email:'ktos@pk.edu.pl', password: 'cos', name: 'Jakis', surname: 'Ktos');
+        $user = new User(email: 'ktos@pk.edu.pl', password: 'cos', name: 'Jakis', surname: 'Ktos');
 
+        if ($this->isPost()) {
+            return $this->login('login');
+        }
 
-       $email = $_POST["email"];
-       $password = $_POST["password"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
-       if ($user->getEmail() !== $email){
+        if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email does not exists']]);
-       }
+        }
 
-       if ($password->getEmail() !== $password){
-        return $this->render('login', ['messages' => ["Wrong password"]]);
-       }
-       
-       return $this->render('users');
+        if ($password->getEmail() !== $password) {
+            return $this->render('login', ['messages' => ["Wrong password"]]);
+        }
+
+        return $this->render('users');
     }
 }
